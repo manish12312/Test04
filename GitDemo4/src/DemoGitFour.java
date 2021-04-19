7c55a4e4e4095771894684ebe0f61fe35ff8dcb2
manish12312 <82791420+manish12312@users.noreply.github.com>
2021-04-19 13:07:44 +0530

    submit m3  and m4 method
    By gIT TEAM

--- GitDemo4/src/DemoGitFour.java
+++ GitDemo4/src/DemoGitFour.java
@@ -21,5 +21,13 @@ public class DemoGitFour
 	{
 		System.out.println(" Hello m2");
 	}
+	public void m3()
+	{
+		System.out.println(" Hello m3");
+	}
+	public void m4()
+	{
+		System.out.println(" Hello m4");
+	}
 
 }
