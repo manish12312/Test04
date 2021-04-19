fbc64f81cfc9b7ed01667a8ab6139f03e60360ef
MANISH <MANISH@DESKTOP-NPV50CU>
2021-04-19 12:38:33 +0530

    Submite By MANISH Mahajan

---
+++ GitDemo4/src/DemoGitFour.java
@@ -0,0 +1,17 @@
+
+public class DemoGitFour 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println(" Hello Demo GIt 4");
+
+	}
+	public void Subtract()
+	{
+		System.out.println(" Hello Subtract");
+	}
+	
+
+}
