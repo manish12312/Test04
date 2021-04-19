baea871969fcd7f5fe7cc69f73cb560686c954fc
MANISH <MANISH@DESKTOP-NPV50CU>
2021-04-19 12:59:47 +0530

    Submited by Manish

--- GitDemo4/src/DemoGitFour.java
+++ GitDemo4/src/DemoGitFour.java
@@ -13,5 +13,13 @@ public class DemoGitFour
 		System.out.println(" Hello Subtract");
 	}
 	
+	public void m1()
+	{
+		System.out.println(" Hello m1");
+	}
+	public void m2()
+	{
+		System.out.println(" Hello m2");
+	}
 
 }
